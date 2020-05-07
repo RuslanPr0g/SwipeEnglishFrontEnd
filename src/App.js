8ce51cf05784ce503f77727e7e87876ec9702ef6
RuslanPr0g <59767834+RuslanPr0g@users.noreply.github.com>
2020-05-07 19:46:39 +0300

    file mjnt

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Navbar from "./components/navbar.component";
 //import ExerciseList from "./components/exercises-list.component";
 import EditExercise from "./components/edit-exercises.component";
 import CreateExercise from "./components/create-exercise.component";
-import CreateUser from "./components/create-user.component";
 import Landing from './components/Landing'
 import Login from './components/Login'
 import Register from './components/Register'
@@ -21,7 +20,6 @@ function App() {
         <Route path="/" exact component = {Landing} />
         <Route path="/edit/:id" component = {EditExercise} />
         <Route path="/create" component = {CreateExercise} />
-        <Route path="/user" component = {CreateUser} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/profile" component={Profile} />
