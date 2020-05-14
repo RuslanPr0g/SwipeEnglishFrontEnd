6a28b1fc930c39bc6d66de2e7e2bde5cf6e484d2
RuslanPr0g <59767834+RuslanPr0g@users.noreply.github.com>
2020-05-14 16:13:27 +0300

    Test Elements added + fix

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,11 +5,6 @@ class Landing extends Component {
   render() {
     return (
       <div className="container">
-        <div className="jumbotron mt-5">
-          <div className="col-sm-8 mx-auto">
-            <h1 className="text-center display-3">WELCOME</h1>
-          </div>
-        </div>
         {localStorage.usertoken ? <CardWord></CardWord> : null}
       </div>
     )
--- src/components/swipe-card-components/Main.js
+++ src/components/swipe-card-components/Main.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react'
 import jwt_decode from 'jwt-decode';
 import axios from 'axios';
 
-// const Word = props => (
-//     <tr>
-//       <td>{props.word.name}</td>
-//       <td>{props.word.difficulty}</td>
-//       <td>{props.word.speech}</td>
-//     </tr>
-//   );
+const Word = props => (
+    <tr className="d-flex justify-content-center">
+      <td>{props.word}</td>
+    </tr>
+  );
 
 class Landing extends Component {
     constructor(props) {
@@ -16,11 +14,13 @@ class Landing extends Component {
 
         this.generateRandomWord = this.generateRandomWord.bind(this)
         this.addWord = this.addWord.bind(this)
+        this.addTest = this.addTest.bind(this)
+        this.wordsList = this.wordsList.bind(this)
 
         const token = localStorage.usertoken
         const decoded = jwt_decode(token)
     
-        this.state = {user: decoded, random_word: null, definition: null, countWords: -1};
+        this.state = {user: decoded, random_word: null, definition: null, countWords: -1, toTest: [], isLimit: false};
     }
 
     addWord() {
@@ -29,6 +29,13 @@ class Landing extends Component {
             .then(function(response) {console.log(response.data)})
         this.generateRandomWord();
       }
+
+    addTest(word) {
+      let new_test = this.state.toTest;
+      new_test.push(word);
+      this.setState({toTest: new_test})
+      this.generateRandomWord()
+    }
 
     generateRandomWord(){
         this.setState({countWords: 0})
@@ -39,6 +46,10 @@ class Landing extends Component {
         .catch((error) => {
           console.log(error);
         })
+    }
+
+    wordsList() {
+      return this.state.toTest.map(word => <Word word={word} key={word} />)
     }
 
   render() {
@@ -55,7 +66,7 @@ class Landing extends Component {
     <div className="container"><p className="blockquote-footer">{this.state.definition}</p>
             <h1 className="text-center display-3">{this.state.random_word}</h1>
             <div className="d-flex justify-content-between">
-            <button className="btn btn-outline-info btn-lg" onClick={() => { this.generateRandomWord() }}>SKIP</button>
+            <button className="btn btn-outline-info btn-lg" onClick={() => { this.addTest(this.state.random_word) }}>TEST</button>
             <button className="btn btn-outline-success btn-lg" onClick={() => { this.addWord()}}>KNOW</button>
             </div>
         </div>
@@ -63,7 +74,25 @@ class Landing extends Component {
 
     return (
       <div className="container">
-        <div className="jumbotron mt-5">
+        <div className="d-flex justify-content-between">
+        <div className="w-25">
+        <table className="table content-table w-75">
+          <thead className="table table-dark">
+            <tr className="tr-main d-flex justify-content-center">
+              <th className="th d-flex justify-content-center">WORDS TO TEST</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.wordsList() }
+          </tbody>
+        </table>
+        {this.state.toTest.length >= 5 ? this.state.isLimit = true : this.state.isLimit = false}
+    {this.state.isLimit ? <button className="btn btn-outline-primary">LET'S GO</button> : <h4 className="btn btn-secondary">{this.state.toTest.length}/5</h4>}
+      </div>
+        <div className="jumbotron mt-5 w-50">
+          { this.state.isLimit ?
+          <h1 className="display-3">Please, pass the test</h1>
+          :
           <div className="col-sm-8 mx-auto">
              <div className="container">
                {this.state.countWords > -1
@@ -74,6 +103,8 @@ class Landing extends Component {
                }
             </div>
           </div>
+          }
+        </div>
         </div>
       </div>
     )
