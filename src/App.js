a56dc6eb999b79c9fc714e185712b27cf19783e3
RuslanPr0g <59767834+RuslanPr0g@users.noreply.github.com>
2020-05-16 14:23:26 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
 
 import Navbar from "./components/navbar.component";
-//import ExerciseList from "./components/exercises-list.component";
-import EditExercise from "./components/edit-exercises.component";
-import CreateExercise from "./components/create-exercise.component";
 import Landing from './components/Landing'
 import Login from './components/login-components/Login'
 import Register from './components/login-components/Register'
@@ -20,8 +17,6 @@ function App() {
         BETA*
         <br/>
         <Route path="/" exact component = {Landing} />
-        <Route path="/edit/:id" component = {EditExercise} />
-        <Route path="/create" component = {CreateExercise} />
         <Route exact path="/words" component = {Dictionary} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
